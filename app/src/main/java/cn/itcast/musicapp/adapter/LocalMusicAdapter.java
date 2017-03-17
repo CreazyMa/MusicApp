8102b45691e26e0491b28a320ea0bfe2f027f978
CreazyMa <1187953762@qq.com>
2017-03-17 14:05:58 +0800

    the code had changed

--- app/src/main/java/cn/itcast/musicapp/adapter/LocalMusicAdapter.java
+++ app/src/main/java/cn/itcast/musicapp/adapter/LocalMusicAdapter.java
@@ -2,6 +2,7 @@ package cn.itcast.musicapp.adapter;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,7 +11,7 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
-import cn.itcast.musicapp.util.ArtworkUtils;
+
 import cn.itcast.musicapp.util.MediaUtils;
 import cn.itcast.musicapp.bean.Mp3Info;
 import cn.itcast.musicapp.R;
@@ -24,10 +25,25 @@ public class LocalMusicAdapter extends RecyclerView.Adapter<LocalMusicAdapter.Vi
     private ArrayList<Mp3Info> mp3Infos;
     private Context context;
 
+    private OnItemClickListener onItemClickListener;
+
     public LocalMusicAdapter(Context context, ArrayList<Mp3Info> mp3Infos) {
         this.context = context;
-        this.mInflater = LayoutInflater.from(context);
         this.mp3Infos = mp3Infos;
+        this.mInflater = LayoutInflater.from(context);
+    }
+
+    //歌曲点击监听接口
+    public interface OnItemClickListener {
+        void onItemClick(int position);
+
+        void onItemLongClick(int position);
+    }
+
+
+
+    public void setOnItemClickListener(OnItemClickListener onItemClickListener) {
+        this.onItemClickListener = onItemClickListener;
     }
 
     @Override
@@ -35,6 +51,31 @@ public class LocalMusicAdapter extends RecyclerView.Adapter<LocalMusicAdapter.Vi
         View view = mInflater.inflate(R.layout.item_music_layout, parent, false);
         ViewHolder viewHolder = new ViewHolder(view);
         return viewHolder;
+    }
+    @Override
+    public void onBindViewHolder(ViewHolder holder, final int position) {
+        Mp3Info mp3Info = mp3Infos.get(position);
+        holder.textView1_title.setText(mp3Info.getTitle());
+        holder.textView2_singer.setText(mp3Info.getArtist());
+        holder.textView3_time.setText(MediaUtils.formatTime(mp3Info.getDuration()));
+        holder.imageView1_icon.setImageBitmap(MediaUtils.getArtWork(context,mp3Info.getId(),mp3Info.getAlbumId(),true,true));
+        if (onItemClickListener!=null){
+            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Log.i("Adapter里面：" ,"+"+position+"+");
+                    onItemClickListener.onItemClick(position);
+                }
+            });
+            holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View view) {
+                    onItemClickListener.onItemLongClick(position);
+                    return false;
+                }
+            });
+        }
+
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
@@ -53,49 +94,14 @@ public class LocalMusicAdapter extends RecyclerView.Adapter<LocalMusicAdapter.Vi
     }
 
 
-    @Override
-    public void onBindViewHolder(ViewHolder holder, final int position) {
-        Mp3Info mp3Info = mp3Infos.get(position);
-        holder.textView1_title.setText(mp3Info.getTitle());
-        holder.textView2_singer.setText(mp3Info.getArtist());
-        holder.textView3_time.setText(MediaUtils.formatTime(mp3Info.getDuration()));
-//        holder.imageView1_icon.setImageBitmap(MediaUtils.getArtwork(context,mp3Info.getId(),mp3Info.getAlbumId(),true,true));
-        holder.imageView1_icon.setImageBitmap(ArtworkUtils.getArtwork(context, mp3Info.getId(), mp3Info.getAlbumId(), true));
-        if (onItemClickListener!=null){
-            holder.itemView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    onItemClickListener.onItemClick(position);
-                }
-            });
-            holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
-                @Override
-                public boolean onLongClick(View view) {
-                    onItemClickListener.onItemLongClick(position);
-                    return false;
-                }
-            });
-        }
 
-    }
 
     @Override
     public int getItemCount() {
         return mp3Infos.size();
     }
 
-    //歌曲点击监听接口
-    public interface OnItemClickListener {
-        void onItemClick(int position);
 
-        void onItemLongClick(int position);
-    }
-
-    private OnItemClickListener onItemClickListener;
-
-    public void setOnItemClickListener(OnItemClickListener onItemClickListener) {
-        this.onItemClickListener = onItemClickListener;
-    }
 
 
 }
