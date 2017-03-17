dedba5d5368f37c1a1d652414652c3273ae78062
CreazyMa <1187953762@qq.com>
2017-03-17 14:09:19 +0800

    some new  methods ware added

--- app/src/main/java/cn/itcast/musicapp/MainActivity.java
+++ app/src/main/java/cn/itcast/musicapp/MainActivity.java
@@ -1,7 +1,14 @@
 package cn.itcast.musicapp;
 
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
@@ -16,36 +23,82 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import cn.itcast.musicapp.adapter.MyFragmentAdapter;
+import cn.itcast.musicapp.bean.Mp3Info;
 import cn.itcast.musicapp.fragment.LocalFragment;
+import cn.itcast.musicapp.service.PlayService;
+import cn.itcast.musicapp.util.MediaUtils;
 import layout.MyFragment;
 import layout.MyFragment.OnFragmentInteractionListener;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener, OnFragmentInteractionListener {
     private Fragment fragOne, fragTwo, fragThree;
-    private List<Fragment> list_fragment;
-    private List<String> list_title;
+    private List<Fragment> list_fragment = null;
+    private List<String> list_title = null;
+    private ViewPager viewPager = null;
+    private TextView tvSongName;
+    private TextView tvSinger;
+    private ImageView ivSongPic;
+    private ImageView ivPlay;
+    private ImageView ivNext;
+    protected PlayService playService;
+    private boolean isBound = false;//是否已经绑定
+    private boolean isPlaying = false;
+    private BroadcastReceiver mReceiver;
+
+
+
+    private ServiceConnection conn = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            PlayService.PlayBinder playBinder = (PlayService.PlayBinder) iBinder;
+            playService = playBinder.getPlayService();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            playService = null;
+            isBound = false;
+        }
+    };
+
+    //绑定服务
+    public void bindPlayService() {
+        if (!isBound) {
+            Intent intent = new Intent(this, PlayService.class);
+            bindService(intent, conn, Context.BIND_AUTO_CREATE);
+            isBound = true;
+        }
+    }
+
+    //解除绑定服务
+    public void unbindPlayService() {
+        if (isBound) {
+            unbindService(conn);
+            isBound = false;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
+        startService(new Intent(this, PlayService.class));
+        bindPlayService();
+
+
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -56,6 +109,10 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
         TabLayout tl = (TabLayout) findViewById(R.id.tabs);
+
+
+
+
 //        t1.addTab(t1.newTab().setText("本地音乐"));
 //        t1.addTab(t1.newTab().setText("网络音乐"));
 //        t1.addTab(t1.newTab().setText("个人中心"));
@@ -70,18 +127,63 @@ public class MainActivity extends AppCompatActivity
         fragTwo = MyFragment.newInstance("网络音乐", "暂时没有");
         fragThree = MyFragment.newInstance("动态", "高级拓展");
 
+
+
         list_fragment = new ArrayList<>();
         //list_fragment.add(fragOne);
         list_fragment.add(localFragment);
         list_fragment.add(fragTwo);
         list_fragment.add(fragThree);
 
-
-        ViewPager vp = (ViewPager)findViewById(R.id.viewPaper);
+        viewPager = (ViewPager)findViewById(R.id.viewPaper);
         MyFragmentAdapter mfa = new MyFragmentAdapter(getSupportFragmentManager(),list_fragment,list_title);
-        vp.setAdapter(mfa);
-        tl.setupWithViewPager(vp);
+        viewPager.setAdapter(mfa);
+        tl.setupWithViewPager(viewPager);
 
+
+        tvSongName = (TextView) findViewById(R.id.text_songName);
+        tvSinger = (TextView) findViewById(R.id.text_singer);
+        ivNext = (ImageView) findViewById(R.id.imageView3_next);
+        ivPlay = (ImageView) findViewById(R.id.imageView2_play_pasue);
+        ivSongPic = (ImageView) findViewById(R.id.songImage);
+
+        ivPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!isPlaying) {//正在播放则暂停
+                    isPlaying = true;
+                    playService.play(getApplicationContext(),0);
+//                    playService.play(first);
+                    if (playService.isPlaying()) {
+                        ivPlay.setImageResource(R.mipmap.uamp_ic_play_arrow_white_48dp);
+                    }
+                } else if (playService.isPlaying()) {//正在播放，暂停
+                    playService.pasue();
+                    ivPlay.setImageResource(R.mipmap.uamp_ic_play_arrow_white_48dp);
+                } else {  //暂停则播放
+                    playService.start();
+                    ivPlay.setImageResource(R.mipmap.uamp_ic_pause_white_48dp);
+                }
+            }
+        });
+        ivNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                playService.next();
+            }
+        });
+
+
+        mReceiver = new MusicChangeRecriver();
+        IntentFilter intentFilter = new IntentFilter(Constant.RECRIVER_MUSIC_CHANGE);
+        registerReceiver(mReceiver, intentFilter);
+
+    }
+    @Override
+    protected void onDestroy() {
+        unregisterReceiver(mReceiver);
+        unbindPlayService();
+        super.onDestroy();
     }
 
     @Override
@@ -144,5 +246,60 @@ public class MainActivity extends AppCompatActivity
     @Override
     public void onFragmentInteraction(Uri uri) {
 
+    }
+
+//    public boolean play(int position){
+//        Mp3Info mp3Info = playService.play(this,position);
+//        tvSongName.setText(mp3Info.getTitle());
+//        tvSinger.setText(mp3Info.getArtist());
+//        ivSongPic.setImageBitmap(MediaUtils.getArtWork(this, mp3Info.getId(), mp3Info.getAlbumId(), true, true));
+//        ivPlay.setImageResource(R.mipmap.uamp_ic_pause_white_48dp);
+//        isPlaying = true;
+//        return true;
+//    }
+
+
+
+    public class Constant {
+        public static final String DEFAULT_MUSIC_TITLE = "MusicApp";
+        public static final String DEFAULT_ARTIST = "郑州轻工业学院";
+        public static final String RECRIVER_MUSIC_CHANGE = "cn.itcast.musicapp";
+    }
+
+    private class MusicChangeRecriver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (Constant.RECRIVER_MUSIC_CHANGE.equals(intent.getAction())) {
+                onPlayStateChanged();
+            }
+        }
+    }
+
+    private void onPlayStateChanged() {
+        Mp3Info music = playService.getMusic();
+        if (music == null) {
+            tvSongName.setText(Constant.DEFAULT_MUSIC_TITLE);
+            tvSinger.setText(Constant.DEFAULT_ARTIST);
+            ivSongPic.setImageResource(R.mipmap.local_ic);
+            ivPlay.setImageResource(R.mipmap.uamp_ic_play_arrow_white_48dp);
+            return;
+        }
+        tvSongName.setText(music.getTitle());
+        tvSinger.setText(music.getArtist());
+        ivSongPic.setImageBitmap(MediaUtils.getArtWork(getApplicationContext(), music.getId(), music.getAlbumId(), true, true));
+        if (playService.isPlaying()) {
+            ivPlay.setImageResource(R.mipmap.uamp_ic_pause_white_48dp);
+        } else {
+            ivPlay.setImageResource(R.mipmap.uamp_ic_play_arrow_white_48dp);
+        }
+
+    }
+
+
+    public boolean play(int position){
+        playService.play(this,position);
+        isPlaying = true;
+        return true;
     }
 }
